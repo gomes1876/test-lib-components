65996ec1d6e123d23c7ad7066cf27d1fb984a403
Gabriel Gomes de Oliveira <gabriel.oliveira@codenapp.com>
2023-02-10 09:32:36 -0300

    teste de reorganização da biblioteca

--- index.js
+++
@@ -1,9 +0,0 @@
-import TButton from './src/components/TButton/index.vue'
-import TInput from './src/components/TInput/index.vue'
-import TTextarea from './src/components/TTextarea/index.vue'
-
-export {
-    TButton,
-    TInput,
-    TTextarea,
-}
