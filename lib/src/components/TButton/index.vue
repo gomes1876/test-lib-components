1a1f4cd4ddc6db78c91c5d376c38e5e8b8a0a235
Gabriel Gomes de Oliveira <gabriel.oliveira@codenapp.com>
2023-02-10 15:15:19 -0300

    select em funcionamento porém sem ícones

--- lib/src/components/TButton/index.vue
+++ lib/src/components/TButton/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="[
+  <button v-if="!dropdown" :class="[
     'button-item',
     'text-semibold',
     disabled && 'disabled',
@@ -10,17 +10,43 @@
     variant,
   
   ]" :style="styleIcon" @click="$emit('click')" :to="{ name: linkPage }">
-    <div class="content-button">
-      <slot name="icon"></slot>
-      <slot></slot>
+<div v-if="!dropdown" class="content-button">
+
+  <div v-if="!dropdown">
+    <slot name="icon">
+    </slot>
+  </div>
+  <slot></slot>
+</div>
+</button>
+<div v-else-if="dropdown">
+  <b-dropdown variant="link" toggle-class="text-decoration-none" no-caret class="d-action-reasons">
+    <template v-slot:button-content>
+      <slot name="icon">
+      </slot>
+    </template>
+    <div v-for="(item, index) of itemsSelect">
+
+      <b-dropdown-item @click="item.click">
+        <span :id="item.title" class="d-itens m-1">
+          {{ item.title }}
+        </span>
+      </b-dropdown-item>
     </div>
-  </button>
+  </b-dropdown>
+</div>
 </template>
 
 <script>
+import { BDropdown, BDropdownItem } from 'bootstrap-vue'
+
+const spans = document.getElementById('item');
+
+
 export default {
   name: "t-button",
   components: {
+    BDropdown, BDropdownItem
   },
   data() {
     return {
@@ -55,12 +81,28 @@ export default {
       type: String,
       default: null,
     },
-    icon: {
-      type: String,
-      default: null
-    }
-
+    dropdown: {
+      type: Boolean,
+      default: false
+    },
+    itemsSelect: {
+      type: Array,
+      default: [
+        {},
+      ]
+    },
   },
+  // mounted() {
+  //   console.log('entra');
+
+  //   this.itemsSelect.forEach((item, index) => {
+  //     console.log(`${this.itemsSelect[index].title}`);
+  //     const element = document.getElementById(item.title + index)
+  //     console.log(element);
+  //     element.innerHTML = this.itemsSelect[index].icon
+  //   })
+
+  // },
   computed: {
     styleIcon() {
       return {
@@ -83,16 +125,12 @@ export default {
       }
       if (this.variant) {
         return this.variant == 'primary' ? '#974900' : this.variant == 'danger' ? '#D32F2F' : this.variant == 'warning' ? '#FF9800' : this.variant == 'success' ? '#4CAF50' : '#1976D2'
-      } 
+      }
 
     }
 
   },
 }
 </script>
-
-
-
-
 
 <style scoped src="./t-button.css" />
