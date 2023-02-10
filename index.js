1587273bb28cb7e2c7751739761b156617861d94
Gabriel Gomes de Oliveira <gabriel.oliveira@codenapp.com>
2023-02-10 09:39:29 -0300

    correção export

---
+++ index.js
@@ -0,0 +1,9 @@
+import TButton from './lib/src/components/TButton/index.vue'
+import TInput from './lib/src/components/TInput/index.vue'
+import TTextarea from './lib/src/components/TTextarea/index.vue'
+
+export {
+    TButton,
+    TInput,
+    TTextarea,
+}
